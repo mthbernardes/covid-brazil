3a48ba93328c034e5a79c2f839bdb22e11bddf73
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2020-04-26 06:49:02 -0300

    add search button and change map provider

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var map = L.map('map',{renderer: L.canvas()}).setView([-9.68489 , -36.3078], 4);
+var map = L.map('map', { renderer: L.canvas(), center: [-9.68489 , -36.3078], zoom: 4})
 var brasil_io_url = "https://brasil.io/api/dataset/covid19/caso/data/?format=json";
 var global_counties;
 
-L.tileLayer('https://api.maptiler.com/maps/darkmatter/{z}/{x}/{y}.png?key=HaCoHE37CXvW8kmOGBV2',{
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',{
     tileSize: 512,
     zoomOffset: -1,
     minZoom: 4,    
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstr    eetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     crossOrigin: true
 }).addTo(map);
 
@@ -44,7 +43,19 @@ function getCases(url){
         });
 }
 
-fetch(window.location.href+"/counties.json")
+map.addControl( new L.Control.Search({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}&countrycodes=br',
+    jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat','lon'],
+    zoom: 12,
+    autoCollapse: true,
+    hideMarkerOnCollapse: true,
+    autoType: false,
+    minLength: 2
+}) );
+
+fetch(window.location.href+"/data/counties.json")
     .then(response => response.json())
     .then(counties => {
         global_counties = counties;
